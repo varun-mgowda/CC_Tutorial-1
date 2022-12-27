cda9dcab39cf13a4d4b5920b75cd92ac3a61e545
varun-mgowda <77537130+varun-mgowda@users.noreply.github.com>
2022-12-27 12:07:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,59 @@ app.listen(port, err => {
     }
     console.log('Server running successfully!!');
 });
+app.post("/api/userLogin", function(req,res){
+   
+//    userLogin.create({
+//     username: req.body.username,
+//     aadhaar: req.body.aadhaar,
+//     voter_id: req.body.voter_id,
+// }).catch(err=>{
+//     console.log(err);
+// });
+userLogin.find({}).then(a => { 
+    //create userlogin(s) db in database elections
+       
+       for (i = 0; i < a.length; i++){
+             
+        //    if(a[i].aadhaar==req.body.aadhaar && a[i].username==req.body.username && a[i].voter_id==req.body.voter_id){
+            if(a[i].voter_id==req.body.voter_id &&  a[i].username==req.body.username  && a[i].aadhaar==req.body.aadhaar && a[i].wallet==req.body.wallet){
+            console.log(a[i].aadhaar);
+             res.send(true);
+             return;
+           }
+         }
+         res.send(false);
+     }).catch(err=>{
+        console.log(err);
+     })
+    
+});
+
+app.get("/api/userLogin", function(req,res){
+   
+   
+    userLogin.find({}).then(a => { //create userlogin(s) db in database elections
+           let final=[];
+           for (i = 0; i < a.length; i++){
+                 
+            //    if(a[i].aadhaar==req.body.aadhaar && a[i].username==req.body.username && a[i].voter_id==req.body.voter_id){
+                if(a[i].voter_id==req.body.voter_id &&  a[i].username==req.body.username ){
+                    final.push({
+                        aadhaar:a[i].aadhaar,
+                        wallet: a[i].wallet,
+                        phone:a[i].phone,
+                    })
+                console.log(a[i].aadhaar);
+                 res.send(final);
+                 return;
+               }
+             }
+            
+         }).catch(err=>{
+            console.log(err);
+         })
+        
+    });
+    
+
+
